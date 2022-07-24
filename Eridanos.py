13592a115526ab2d786bb85ffd7c98bacf05f0fc
gundlacc <77037414+gundlacc@users.noreply.github.com>
2022-07-24 16:35:33 -0600

    Add files via upload

---
+++ Eridanos.py
@@ -0,0 +1,8 @@
+from planet import Planet
+
+class Asterope (Planet):
+    def __init__(self):
+        self.planet_name = 'Asterope'
+        self.intro_text_long = 'Welcome to Asterope'
+        self.intro_text_short = 'Welcome back to Asterope! '
+        self.visited = False
