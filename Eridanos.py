93610f3a217e5d6ce5b2f0e1cdd550d82be86fd9
gundlacc <77037414+gundlacc@users.noreply.github.com>
2022-08-08 22:36:39 -0600

    Update Eridanos.py

--- Eridanos.py
+++ Eridanos.py
@@ -22,6 +22,7 @@ class Eridanos(planet.Planet):
         self.placement = 0
         self.accomplished = False
         self.tool = False
+        self.visited = False
 
     def query_NLP(self, text):
         """Placeholder function to query natural language parser."""
@@ -41,6 +42,15 @@ class Eridanos(planet.Planet):
 
         self.data[object[1]](object[0])
 
+        return
+
+    def enter_planet(self):
+        
+        if self.visited == False:
+            print('Plunging cliffs, strange flora, roaring rapids, and the faint smell of sulphur. Welcome to Eridanos./n You have arrived on the top of a medium sized cliffside, but in the distance you can see something that reminds you of a/n collosal termite mound. You start down a trail leading down into ravine that looks like it will take you to the mound, and there appear/n to be several caves along the path.')
+            self.visited = True
+        elif self.visited == True:
+            print("Welcome back to Eridanos! You see the path to the ravine still in front of you as well as the termite mound structure and the ravine caves.")
         return
 
     def ravine_path(self, action):
@@ -66,6 +76,9 @@ class Eridanos(planet.Planet):
         if action.lower() == 'explore':
             print("You go over to the river itself to see what you might be able to find.")
             self.placement += 1
+        elif action.lower() == 'return':
+            print("You head back toward the ravine path to your ship.")
+            self.placement -= 1
 
         return
    
@@ -79,6 +92,11 @@ class Eridanos(planet.Planet):
         if action.lower() == 'grab':
             print("You reach into the river and pull out a tool that looks somewhat like a silver pickaxe. Definitely keeping this.")
             self.tool = True
+        elif action.lower() == 'continue':
+            print("You have to get back to the river path to continue on.")
+        elif action.lower() == 'return':
+            print("You head back toward the river path.")
+            self.placement -= 1
 
 
         return
@@ -93,6 +111,9 @@ class Eridanos(planet.Planet):
         if action.lower() == 'follow':
             print("You continue to follow the path until you make it to the termite mound itself.")
             self.placement += 1
+        elif action.lower() == 'return':
+            print("You head back toward the part of the path with the river access point.")
+            self.placement -= 1
 
         return
 
@@ -106,10 +127,12 @@ class Eridanos(planet.Planet):
         if action.lower() == 'left':
             print("You enter the left side cave of the mound.")
             self.placement += 1
-
-        if action.lower() == 'right':
+        elif action.lower() == 'right':
             print("You enter the right side cave of the mound.")
             self.placement += 2
+        elif action.lower() == 'return':
+            print("You head back toward the river path.")
+            self.placement -= 1
 
         return
 
@@ -126,6 +149,9 @@ class Eridanos(planet.Planet):
             elif self.tool == True:
                 print("After putting the pickaxe into the opening, a fossilized claw comes out. THis is something worth noting and keeping.")
                 self.accomplished = True
+        elif action.lower() == 'return':
+            print("You head back toward the clearing with your ship.")
+            self.placement -= 1
         return
 
     def termite_right_cave(self, action):
@@ -134,10 +160,9 @@ class Eridanos(planet.Planet):
         if self.placement != 6:
             print("Invalid action")
             return
-
-        if action.lower() == 'return':
+        elif action.lower() == 'return':
             print("There's clearly nothing here. Better go back to the main entrance.")
-            self.placement -= 1
+            self.placement -= 2
 
         return
     
